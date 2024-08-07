3a16f2867f7a2b4cf8fa36ceab07cebb215619bf
oversampling <chan1992241@gmail.com>
2024-08-07 11:58:27 +0800

    Feat(CICD): Pipeline to plan infrastructure in HW cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,17 @@ variable "password" {
 
 variable "project_ID" {
   type = string
+  default = "0"
 }
 
 variable "availability_zone" {
   type = string
   default = "ap-southeast-3"
+}
+
+variable "environment" {
+  type = string
+  default = "test"
 }
 
 provider "huaweicloud" {
@@ -25,52 +31,132 @@ provider "huaweicloud" {
   secret_key = var.secret_key
 }
 
-resource "huaweicloud_compute_instance" "likecard-ecs" {
-  name = "likecard-ecs-1"
-  image_name = "Huawei Cloud EulerOS 2.0 Standard 64 bit"
-  flavor_id = "s3.xlarge.2"
-  admin_pass = var.password
-  system_disk_type = "SAS"
-  system_disk_size = 40
-  network {
-    uuid = huaweicloud_vpc_subnet.jenkins-subnet.id
+terraform {
+  backend "s3" {
+    # Reference: https://registry.terraform.io/providers/huaweicloud/hcso/latest/docs/guides/remote-state-backend
+    bucket   = "ais-test-jy"
+    key      = "terraform.tfstate"
+    region   = "ap-southeast-3"
+    endpoint = "https://obs.ap-southeast-3.myhuaweicloud.com"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
-resource "huaweicloud_vpc" "likecard-vpc" {
-  name                  = "likecard-vpc-1"
+resource "huaweicloud_vpc" "cce-vpc" {
+  name                  = "cce-vpc"
   cidr                  = "192.168.0.0/16"
   enterprise_project_id = var.project_ID
 }
 
-resource "huaweicloud_vpc_subnet" "jenkins-subnet" {
-  name       = "jenkins-subnet-1"
+resource "huaweicloud_vpc_subnet" "cce-subnet" {
+  name       = "cce-subnet"
   cidr       = "192.168.0.0/16"
   gateway_ip = "192.168.0.1"
-
-  primary_dns   = "100.125.1.250"
-  secondary_dns = "100.125.21.250"
-  vpc_id        = huaweicloud_vpc.likecard-vpc.id
+  vpc_id        = huaweicloud_vpc.cce-vpc.id
 }
 
-resource "huaweicloud_vpc_eip" "likecard-eip" {
-  name = "likecard-eip-1"
+resource "huaweicloud_vpc_eip" "cce-control-plane-eip" {
   publicip {
     type = "5_bgp"
   }
   bandwidth {
-    name        = "test"
-    size        = 5
+    name        = "mybandwidth"
+    size        = 10
     share_type  = "PER"
     charge_mode = "traffic"
   }
 }
 
-resource "huaweicloud_compute_eip_associate" "likecard-associated" {
-  public_ip   = huaweicloud_vpc_eip.likecard-eip.address
-  instance_id = huaweicloud_compute_instance.likecard-ecs.id
+resource "random_string" "random_suffix" {
+  length  = 8
+  special = false
 }
 
-output "likecard-IP" {
-  value = huaweicloud_vpc_eip.likecard-eip.publicip
+resource "huaweicloud_cce_cluster" "huawei-cce" {
+  name                   = "huawei-cce-${var.environment}-${random_string.random_suffix.result}"
+  flavor_id              = "cce.s1.small" # 50 Nodes
+  vpc_id                 = huaweicloud_vpc.cce-vpc.id
+  subnet_id              = huaweicloud_vpc_subnet.cce-subnet.id
+  container_network_type = "overlay_l2"
+  eip                    = huaweicloud_vpc_eip.cce-control-plane-eip.address
+  cluster_version        = "v1.29"
+  enterprise_project_id  = var.project_ID
+}
+
+
+data "huaweicloud_availability_zones" "cce-az" {
+  region = "ap-southeast-3"
+}
+
+resource "huaweicloud_cce_node" "cce-node" {
+  cluster_id        = huaweicloud_cce_cluster.huawei-cce.id
+  name              = "cce-node--${var.environment}-${random_string.random_suffix.result}"
+  flavor_id         = "c7n.xlarge.2"
+  availability_zone = data.huaweicloud_availability_zones.cce-az.names[0]
+  password          = var.password
+  runtime           = "docker"
+  root_volume {
+    size       = 40
+    volumetype = "SAS"
+  }
+  data_volumes {
+    size       = 100
+    volumetype = "SAS"
+  }
+}
+
+# variable "cluster_id" {
+#   type = string
+#   default = "33e24729-1dc9-11ef-abd5-0255ac100039"
+# }
+
+variable "addon_name" {
+  type = string
+  default = "cie-collector"
+}
+
+variable "addon_version" {
+  type = string
+  default = "3.10.1"
+}
+
+data "huaweicloud_cce_addon_template" "cie-collector" {
+  cluster_id = huaweicloud_cce_cluster.huawei-cce.id
+  name       = var.addon_name
+  version    = var.addon_version
+}
+
+resource "huaweicloud_cce_addon" "cie-collector" {
+  cluster_id    = huaweicloud_cce_cluster.huawei-cce.id
+  template_name = var.addon_name
+  # version       = var.addon_version
+  values {
+    basic_json  = jsonencode(jsondecode(data.huaweicloud_cce_addon_template.cie-collector.spec).basic)
+    custom_json = jsonencode(merge(
+      jsondecode(data.huaweicloud_cce_addon_template.cie-collector.spec).parameters.custom,
+      {
+        cluster_id = huaweicloud_cce_cluster.huawei-cce.id
+        tenant_id  = var.project_ID
+        retention  = "7d"
+        enable_nodeAffinity=false
+        shards = 2
+        # storage_class="csi-obs"
+        # storage_size="15Gi"
+        # storage_type="STANDARD"
+        supportServerModeSharding=true
+        highAvailability=true
+        scrapeInterval="16s"
+        enable_grafana=false
+        
+      }
+    ))
+    flavor_json = jsonencode(
+      jsondecode(data.huaweicloud_cce_addon_template.cie-collector.spec).parameters.flavor7,
+    )
+  }
 }
