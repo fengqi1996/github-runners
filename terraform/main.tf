07911469d0fa8be65cd3911e71969b3f31b0672a
oversampling <chan1992241@gmail.com>
2024-10-18 21:09:33 +0800

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,7 +89,7 @@ resource "huaweicloud_cce_cluster" "huawei-cce" {
   vpc_id                 = huaweicloud_vpc.cce-vpc.id
   subnet_id              = huaweicloud_vpc_subnet.cce-subnet.id
   container_network_type = "overlay_l2"
-  eip                    = huaweicloud_vpc_eip.cce-control-plane-eip.address
+  # eip                    = huaweicloud_vpc_eip.cce-control-plane-eip.address
   cluster_version        = "v1.28"
   enterprise_project_id  = huaweicloud_enterprise_project.itcp-microservice-staging.id
   masters {
